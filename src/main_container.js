fee35d1f6a5073936ed0113ce30904960504439e
swsatheesh <18531982+swsatheesh@users.noreply.github.com>
2018-08-09 14:21:04 +0530

    corrections.

--- src/main_container.js
+++ src/main_container.js
@@ -1,17 +1,34 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
+
+import { Switch, Route } from "react-router-dom";
+
+import MainMenu from "kqilp_components/route_component/main_menu";
+import AssignmentContainer from "kqilp_components/assignment/assignment.container";
+
+const RouteAndComp = () => (
+  <Switch>
+    <Route exact path="/" component={MainMenu} />
+    <Route path="/assignment/foodChain" component={AssignmentContainer} />
+    <Route path="/assignment/backgroundInfo" component={AssignmentContainer} />
+    <Route
+      path="/assignment/habitatAdaptation"
+      component={AssignmentContainer}
+    />
+    <Route path="/assignment/lifeCycle" component={AssignmentContainer} />
+    <Route path="/assignment/anatomy" component={AssignmentContainer} />
+    <Route path="/assignment/dissectionLab" component={AssignmentContainer} />
+    <Route path="/assignment/simulation" component={AssignmentContainer} />
+    <Route path="/assignment/evaluation" component={AssignmentContainer} />
+    <Route path="/assignment/labNotes" component={AssignmentContainer} />
+  </Switch>
+);
 
 class MainContainer extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <RouteAndComp />
       </div>
     );
   }
